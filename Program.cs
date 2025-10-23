8af6efed37a2046dd28a743076ebe526a8005213
joaovieira0087 <joaovieira0087@gmail.com>
2025-10-22 22:45:04 -0300

    chore: primeiro commit do TextEditor

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        static void Menu()
+        {
+            Console.WriteLine("Text Editor Menu:");
+            Console.WriteLine("1. Open Document");
+            Console.WriteLine("2. Create New Document");
+            Console.WriteLine("0. Exit");
+            Console.Write("Select an option: ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: OpenDocument(); break;
+                case 2: CreateNewDocument(); break;
+
+                default: Menu(); break;
+            }
+
+            static void OpenDocument()
+            {
+
+            }
+            
+            static void CreateNewDocument()
+            {
+
+            }
+        }
+    }
+}
